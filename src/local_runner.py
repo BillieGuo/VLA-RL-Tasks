ddd2328fb86215676349289670a22403f2480dab
BillieGuo <baoguo0816@gmail.com>
2025-05-15 19:56:04 +0800

    update ignore

--- src/local_runner.py
+++ src/local_runner.py
@@ -3,14 +3,21 @@ import gymnasium as gym
 import time
 from mani_skill.utils.wrappers.record import RecordEpisode
 # TODO: Change this to import your env
-from card_stack_env import CardStackEnv
+# from tasks.JunhaoChen.card_stack_env import CardStackEnv
+from tasks.BaoGuo.traj_door_env import TrajDoorEnv
+from tasks.BaoGuo.test_env import TestEnv
 
 def generate_videos(n_episodes=10, max_steps_per_episode=100, video_dir="card_stack_videos"):
     """
     Generate and save videos of random agent interactions in the CardStack environment.
     """
     # TODO: Change this to make your env
-    env = gym.make("CardStack-v1", obs_mode="state", render_mode="rgb_array")
+    task_name = "TrajDoor"
+    video_dir = task_name + "_videos"
+    version = "v1"
+    env = gym.make(f"{task_name}-{version}", render_mode="rgb_array")  # Use the correct environment name
+    
+    # env = gym.make("CardStack-v1", obs_mode="state", render_mode="rgb_array")
     video_dir = os.path.join(video_dir, time.strftime("%Y%m%d-%H%M%S"))
     os.makedirs(video_dir, exist_ok=True)
 
